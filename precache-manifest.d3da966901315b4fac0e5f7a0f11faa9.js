60797f2de427f6f542a58c4b088dc121fbc42310
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-21 16:13:16 -0500

    Updates

---
+++ precache-manifest.d3da966901315b4fac0e5f7a0f11faa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dbea165b57db224e14f84f18dbd024",
+    "url": "/azimut-frontend/index.html"
+  },
+  {
+    "revision": "a19c076118f3f0d9309f",
+    "url": "/azimut-frontend/static/css/main.424deabf.chunk.css"
+  },
+  {
+    "revision": "4d412e9f7def425cf797",
+    "url": "/azimut-frontend/static/js/2.8a1fce31.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/azimut-frontend/static/js/2.8a1fce31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19c076118f3f0d9309f",
+    "url": "/azimut-frontend/static/js/main.e5dcdf68.chunk.js"
+  },
+  {
+    "revision": "c3efb03d896c82ba216e",
+    "url": "/azimut-frontend/static/js/runtime-main.32db9b82.js"
+  }
+]);
