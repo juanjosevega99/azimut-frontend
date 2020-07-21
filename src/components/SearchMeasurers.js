f6f87ef3ea79e12b3131aa0fc9411e824db9ed40
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-21 13:05:07 -0500

    render working

--- src/components/SearchMeasurers.js
+++ src/components/SearchMeasurers.js
@@ -1,66 +1,6 @@
 import React, { Component } from 'react'
-import axios from 'axios'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
 
-export default class CreateNote extends Component {
-
-  state = {
-    users: [],
-    userSelected: '',
-    title: '',
-    content: '',
-    date: new Date(),
-    editing: false,
-    _id: ''
-  }
-
-  async componentDidMount() {
-    const res = await axios.get('http://localhost:4000/api/users')
-    if (res.data.length > 0) {
-      this.setState({
-        users: res.data.map(user => user.username),
-        userSelected: res.data[0].username
-      })
-      if (this.props.match.params.id) {
-        const res = await axios.get('http://localhost:4000/api/notes/' + this.props.match.params.id)
-        this.setState({
-          title: res.data.title,
-          content: res.data.content,
-          date: new Date(res.data.date),
-          userSelected: res.data.author,
-          editing: true,
-          _id: this.props.match.params.id
-        })
-      }
-    }
-  }
-
-  onSubmit = async (e) => {
-    e.preventDefault()
-    const newNote = {
-      title: this.state.title,
-      content: this.state.content,
-      date: this.state.date,
-      author: this.state.userSelected,
-    }
-    if (this.state.editing) {
-      await axios.put('http://localhost:4000/api/notes/' + this.state._id, newNote)
-    } else {
-      await axios.post('http://localhost:4000/api/notes', newNote)
-    }
-    window.location.href = '/'
-  }
-
-  onInputChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
-
-  onChangeDate = date => {
-    this.setState({ date })
-  }
+export default class SearchMeasurers extends Component {
 
   render() {
     return (
